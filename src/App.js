790d801f3fbcdd8efb25407725a9f7ee95196599
Joshua Raimer <joshraimer@gmail.com>
2023-06-26 11:51:22 -0500

    Initial commit to the repo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in 5 days or less
         </a>
       </header>
     </div>
